1e19ce09eb58046d7381fcadbdc0f2b2fbd5b379
Fayzulla8387 <xojayevfayzulla7@gmail.com>
2022-12-01 15:27:04 +0500

    jhgf

--- index.php
+++ index.php
@@ -1,19 +1,13 @@
 <?php
 //weather bot
 $e_message = "! Xatolik \n";
-
+require_once  "Telegram.php";
 try {
 $telegram = new Telegram("5915718443:AAGsEKZF4DJ-5GONc0yvIEq4fUtLR4okYnw");
 $chat_id = $telegram->ChatID();
 $text = $telegram->Text();
 $start = "Assalomu alaykum men ob-havo botiman. Men sizga dunyoning istalgan shahrining ob-havosini ayta olaman. Faqat shahar nomini yozing, men sizga ob-havoni aytib beraman. Masalan, 'London' deb yozing va men sizga Londonning ob-havosini aytib beraman.
  Buyruqlar ro'yxatini ko'rish uchun 'Yordam' ni bosing.";
-$help = "/help";
-$telegram->sendMessage([
-    'chat_id' => $chat_id,
-    'text' => $text,
-]);
-
 switch ($text) {
     case "/start":
         start();
